0f38496a23ba8af98d71560a3f722f8014388c66
Vivek205 <ichbinvivek@gmail.com>
2021-09-01 12:24:53 +0530

    w33 solution

--- 2021/w33/solutions/solution.js
+++ 2021/w33/solutions/solution.js
@@ -1,7 +1,24 @@
-const buildPyramid = (floors, character) => {
+const buildPyramid = (floors, character = "*") => {
   // your solution here
-
-  return undefined;
+  if (floors === 1) {
+    return [character];
+  }
+  let i = 0;
+  let result = [];
+  let strLength = (floors - 1) * 2 + 1;
+  const char = " ".repeat(strLength);
+  for (; i < floors; i++) {
+    if (i + 1 === floors) {
+      result[i] = character.repeat(strLength);
+      break;
+    }
+    const filler = char.slice(0, floors - i - 1);
+    const charLength = 2 * i + 1;
+    result[i] = filler + character.repeat(charLength) + filler;
+  }
+  return result;
 };
+
+console.log(buildPyramid(6));
 
 module.exports = buildPyramid;
